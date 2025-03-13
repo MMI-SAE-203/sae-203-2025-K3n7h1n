709632c726311e2cfc2f790afb65f15997f772f2
Enzo Locatelli <enzort70290@gmail.com>
2025-03-13 16:34:50 +0100

    fix

--- backend.mjs
+++ backend.mjs
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
-const pb = new PocketBase('http://127.0.0.1:8090') ;
+const pb = new PocketBase('https://tpsae203.enzo-locatelli.fr:443') ;
 
 export async function films() {
     const films = await pb.collection('Films').getFullList();
